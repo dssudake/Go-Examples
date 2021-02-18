04565bf89038168d520c4b7b85aa3cc430d21aa7
dssudake <darshansudake555@gmail.com>
2021-02-18 13:47:43 +0530

    Hello World!

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Go-Examples
+# Go Examples
+
 Example programs from Go programming language
+
+Go commands :
+
+```console
+$ go version - Print Go version
+$ go build   - Compile packages and dependencies
+$ go install - Compile and install packages and dependencies
+$ go run     - Compile and run Go program
+$ go get     - Add dependencies to current module
+$ go test    - Test package code using unit tests
+```
---
+++ basic/hello.go
@@ -0,0 +1,15 @@
+// first program to print classic "Hello World!" message
+
+// Every go file must start with the "package pkg_name" statement
+package main
+
+// fmt package is used to print text to the standard output
+import "fmt"
+
+// main function should always reside in the main package
+// It is an entrypoint of application
+func main() {
+
+	//Println function of the fmt package is used to write text to the standard output
+	fmt.Println("👋🌏 \nHello World!")
+}
